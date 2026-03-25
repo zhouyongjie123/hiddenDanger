8ceb808fb2925d360870bd14d6235299af06dd0d
冰咖啡 <z1876849563@163.com>
2026-03-25 13:19:35 +0800

    初始化hiddenRisk模块

---
+++ service/hiddenRisk-service/src/main/java/com/zyj/hiddendanger/risk/HiddenRiskApplication.java
@@ -0,0 +1,29 @@
+package com.zyj.hiddendanger.risk;
+
+import jakarta.annotation.Resource;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.event.EventListener;
+
+import java.util.Optional;
+
+@SpringBootApplication(scanBasePackages = "com.zyj.hiddendanger")
+public class HiddenRiskApplication {
+    @Resource
+    private ConfigurableApplicationContext applicationContext;
+
+    public static void main(String[] args) {
+        SpringApplication.run(HiddenRiskApplication.class, args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void onApplicationReady() {
+        String applicationName = Optional
+                .ofNullable(applicationContext.getEnvironment()
+                                              .getProperty("spring.application.name"))
+                .orElse("");
+        System.out.println(applicationName + " started ✧⁺⸜(๑˙▾˙๑)⸝⁺✧");
+    }
+}
